086de2bbd7dce74f979aee5f9fa64debeccd5392
R0shina <84459945+R0shina@users.noreply.github.com>
2022-06-25 11:48:12 +0545

    Update file.php

--- file.php
+++ file.php
@@ -1,32 +1,15 @@
-<?php
+ <?php
 
-// Function to find the difference
-// between two dates.
-function dateDiffInDays($date1, $date2)
-{
-   
-$date1 = DateTime::createFromFormat('m-d-Y', $date1);
-$date2 = DateTime::createFromFormat('m-d-Y', $date2);
-//echo $date->format('Y-m-d');
-	// Calculating the difference in timestamps
-	//$diff = strtotime($date2) - strtotime($date1);
-    $diff = $date2->diff($date1);
-    return $diff;
-	// 1 day = 24 hours
-	// 24 * 60 * 60 = 86400 seconds
-	//return abs(round($diff / 86400));
+
+function dateDifference($from, $to){
+	$date1 = DateTime::createFromFormat('m-d-Y', $from);
+	 $date2 = DateTime::createFromFormat('m-d-Y', $to);
+	$diff = $date1->diff($date2)->format("%r%a") + 1; 
+
+	 return $diff;
 }
 
-// Start date
-$date1 = "06-22-2022";
 
-// End date
-$date2 = "06-30-2022";
-
-// Function call to find date difference
-$dateDiff = dateDiffInDays($date1, $date2);
-
-// Display the result
-printf("Difference between two dates: "
-	. $dateDiff . " Days ");
-?>
+//$date1 = "06-21-2022";
+//$date2 = "06-23-2022";
+//echo (dateDifference($date1, $date2));
